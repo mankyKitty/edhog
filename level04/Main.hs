76e2142298d48fc9d7c26b779665b670f3034980
Sean Chalmers <sclhiannan@gmail.com>
2019-01-31 13:12:00 +1000

    Update test group name

--- level04/Main.hs
+++ level04/Main.hs
@@ -4,7 +4,7 @@ import CoffeeMachineTests (stateMachineTests)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 tests :: TestTree
-tests = testGroup "Coffee Machine - Level 03" [stateMachineTests]
+tests = testGroup "Coffee Machine - Level 04" [stateMachineTests]
 
 main :: IO ()
 main = defaultMain tests
